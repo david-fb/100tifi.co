2c233f57618f0ac8da7624aab6d3bd6c25116aa7
David Basto <davidbasto01@gmail.com>
2021-06-07 21:40:38 -0500

    Final del curso platzi spa

---
+++ src/templates/Header.js
@@ -0,0 +1,17 @@
+const Header = () =>{
+    const view = `
+        <div class="Header-main">
+            <div class="Header-logo">
+                <a href="/">100tifi.co</a>
+            </div>
+        
+            <div class="Header-nav">
+                <a href="#/about/">About</a>
+            </div>
+        </div>
+    
+    `
+    return view
+}
+
+export default Header
